d8338ed95b71b4b14e3c87dfa13f7a5d03c46a3f
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-10-17 10:32:58 +0300

    test size

--- lib/src/test/kotlin/sp/kx/hashes/RealHashesTest.kt
+++ lib/src/test/kotlin/sp/kx/hashes/RealHashesTest.kt
@@ -1,10 +1,19 @@
 package sp.kx.hashes
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.util.HexFormat
 
 internal class RealHashesTest {
+    @Test
+    fun sizeTest() {
+        assertEquals(16, RealHashes(algorithm = "MD5").size)
+        assertEquals(20, RealHashes(algorithm = "SHA1").size)
+        assertEquals(32, RealHashes(algorithm = "SHA256").size)
+        assertEquals(64, RealHashes(algorithm = "SHA512").size)
+    }
+
     @Test
     fun digestTest() {
         val hashes = RealHashes(algorithm = "MD5")
