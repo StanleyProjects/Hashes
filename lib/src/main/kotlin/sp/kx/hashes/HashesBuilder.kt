848478190d141e5084b8b88a7b819b3bad6285cb
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-23 17:27:18 +0300

    HashesBuilder

---
+++ lib/src/main/kotlin/sp/kx/hashes/HashesBuilder.kt
@@ -0,0 +1,20 @@
+package sp.kx.hashes
+
+import java.security.MessageDigest
+
+class HashesBuilder private constructor(algorithm: String) {
+    private val md = MessageDigest.getInstance(algorithm)
+
+    fun update(encoded: ByteArray): HashesBuilder {
+        md.update(encoded)
+        return this
+    }
+
+    fun digest(encoded: ByteArray): ByteArray {
+        return md.digest(encoded)
+    }
+
+    fun digest(): ByteArray {
+        return md.digest()
+    }
+}
