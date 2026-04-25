9d20960497fd2b6ca395a7fc4fec9065b1e136c3
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-25 13:41:46 +0300

    test

--- lib/src/test/kotlin/sp/kx/hashes/HashesTest.kt
+++ lib/src/test/kotlin/sp/kx/hashes/HashesTest.kt
@@ -2,15 +2,22 @@ package sp.kx.hashes
 
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import java.util.HexFormat
+import java.security.MessageDigest
 
 internal object HashesTest {
     @Test
-    fun sha256DigestTest() {
+    fun digestTest() {
         val encoded = "foo bar baz".toByteArray(Charsets.UTF_8)
-        val hex = "dbd318c1c462aee872f41109a4dfd3048871a03dedd0fe0e757ced57dad6f2d7"
-        val expected = HexFormat.of().parseHex(hex)
-        val actual = Hashes.SHA256.digest(encoded = encoded)
-        assertTrue(expected.contentEquals(actual))
+        mapOf(
+            "md5" to Hashes.MD5,
+            "sha1" to Hashes.SHA1,
+            "sha256" to Hashes.SHA256,
+            "sha512" to Hashes.SHA512,
+        ).forEach { (algorithm, hashes) ->
+            val md = MessageDigest.getInstance(algorithm)
+            val expected = md.digest(encoded)
+            val actual = hashes.digest(encoded = encoded)
+            assertTrue(expected.contentEquals(actual))
+        }
     }
 }
